793e60a99100d11caaa1419c356e6c9b59ea49b7
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-15 11:15:41 -0400

    Hello my name is

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Andrew"
+  end
+
+  get '/hometown' do
+    "My hometown is Kings Park"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Stars"
+  end
 end
